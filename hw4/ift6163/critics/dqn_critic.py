1b2c97ca50f1def73c5c7517e463661de527aeee
sachaMorin <morin.sacha@gmail.com>
2022-03-18 13:49:51 -0400

    Hw4 draft

--- hw4/ift6163/critics/dqn_critic.py
+++ hw4/ift6163/critics/dqn_critic.py
@@ -91,8 +91,8 @@ class DQNCritic(BaseCritic):
         qa_t_values = self.q_net(ob_no)
         q_t_values = torch.gather(qa_t_values, 1, ac_na.unsqueeze(1)).squeeze(1)
         
-        # TODO compute the Q-values from the target network 
-        qa_tp1_values = TODO
+        # TODO (Done) compute the Q-values from the target network
+        qa_tp1_values = self.q_net_target(next_ob_no)
 
         if self.double_q:
             # You must fill this part for Q2 of the Q-learning portion of the homework.
@@ -104,10 +104,10 @@ class DQNCritic(BaseCritic):
         else:
             q_tp1, _ = qa_tp1_values.max(dim=1)
 
-        # TODO compute targets for minimizing Bellman error
+        # TODO (Done) compute targets for minimizing Bellman error
         # HINT: as you saw in lecture, this would be:
             #currentReward + self.gamma * qValuesOfNextTimestep * (not terminal)
-        target = TODO
+        target = reward_n + self.gamma * q_tp1 * (1- terminal_n)
         target = target.detach()
 
         assert q_t_values.shape == target.shape
--- hw4/ift6163/policies/argmax_policy.py
+++ hw4/ift6163/policies/argmax_policy.py
@@ -12,6 +12,8 @@ class ArgMaxPolicy(object):
         else:
             observation = obs[None]
         
-        ## TODO return the action that maxinmizes the Q-value 
+        ## TODO (Done) return the action that maxinmizes the Q-value
         # at the current observation as the output
+        q_val = self.critic(observation)
+        action = q_val.argmax(dim=1)
         return action.squeeze()
